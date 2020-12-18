5f80d018c5be4fbb133f0fe2643bc8741d96b9be
samkitJain <samkitjain@cdnsol.com>
2020-12-18 21:06:41 +0530

    FirstCommit

---
+++ src/main/java/driverFactory/DriverFactory.java
@@ -0,0 +1,55 @@
+package driverFactory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class DriverFactory {
+    public WebDriver driver;
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    /**
+     * This method is used to initialize the thradlocal driver on the basis of given
+     * browser
+     *
+     * @param browser
+     * @return this will return tldriver.
+     */
+    public WebDriver init_driver(String browser) {
+
+        System.out.println("browser value is: " + browser);
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                tlDriver.set(new ChromeDriver());
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                tlDriver.set(new FirefoxDriver());
+                break;
+            case "safari":
+                tlDriver.set(new SafariDriver());
+                break;
+            default:
+                System.out.println("Please pass the correct browser value: " + browser);
+                break;
+        }
+
+        getDriver().manage().deleteAllCookies();
+        getDriver().manage().window().maximize();
+        return getDriver();
+
+    }
+
+    /**
+     * this is used to get the driver with ThreadLocal
+     *
+     * @return
+     */
+    public static synchronized WebDriver getDriver() {
+        return tlDriver.get();
+    }
+}
---
+++ src/main/java/pageObject/LoginPage.java
@@ -0,0 +1,44 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private final WebDriver driver;
+
+    // 1. By Locators: OR
+    private final By emailId = By.id("email");
+    private final By password = By.id("passwd");
+    private final By signInButton = By.id("SubmitLogin");
+    private final By forgotPwdLink = By.linkText("Forgot your password?111");
+
+    // 2. Constructor of the page class:
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // 3. page actions: features(behavior) of the page the form of methods:
+
+    public String getLoginPageTitle() {
+        return driver.getTitle();
+    }
+
+    public boolean isForgotPwdLinkExist() {
+        return driver.findElement(forgotPwdLink).isDisplayed();
+    }
+
+    public void enterUserName(String username) {
+        driver.findElement(emailId).sendKeys(username);
+    }
+
+    public void enterPassword(String pwd) {
+        driver.findElement(password).sendKeys(pwd);
+    }
+
+    public void clickOnLogin() {
+        driver.findElement(signInButton).click();
+    }
+
+
+
+}
